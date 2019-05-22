733cf74db238adf9873e56135c77bc51725df05d
JiangDunchun <jiangdunchun@outlook.com>
2019-05-23 00:20:58 +0800

    add the blog load

---
+++ blogs/blogs.js
@@ -0,0 +1,12 @@
+BlogsCatalog = [
+    {
+        "name":"Some Tips of Using InfluxDB",
+        "time":"21/5/2019",
+        "path":"Influx.md"
+    },
+    {
+        "name":"What can we do by using WAMPFramework",
+        "time":"9/5/2019",
+        "path":"wamp.md"
+    }
+]
