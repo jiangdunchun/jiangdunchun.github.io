18a7397dd7e283abf46b655bd2fd6efe781d1989
JiangDunchun <jiangdunchun@outlook.com>
2019-05-24 22:57:38 +0800

    reconstrcut the index.html

---
+++ json/resume.js
@@ -0,0 +1,26 @@
+ResumeCatalog = [
+    {
+        "title":"Research Engineer ",
+        "time":"Jun/2019-Now",
+        "org":"Wuhan VRMaker Technology Co., Ltd.",
+        "desc":[
+            "Research on how VR & AR meet with IoT.",
+        ]
+    },
+    {
+        "title":"Master of Engineering",
+        "time":"Sep/2015-Jun/2018",
+        "org":"Wuhan University of Technology",
+        "desc":[
+            "Major in Mechanical and Electrical Engineering.",
+        ]
+    },
+    {
+        "title":"Bachelor of Engineering",
+        "time":"Sep/2011-Jun/2015",
+        "org":"Wuhan University of Technology",
+        "desc":[
+            "Major in Mineral Processing Engineer.",
+        ]
+    }
+]
