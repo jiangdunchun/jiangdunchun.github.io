345187da5053420f221d8580a6bab299375872af
jiangdunchun <jiangdunchun@outlook.com>
2019-05-25 17:48:01 +0800

    use the md as blog now

--- json/blogs.js
+++ json/blogs.js
@@ -2,11 +2,11 @@ BlogsCatalog = [
     {
         "name":"Some Tips of Using InfluxDB",
         "time":"21/5/2019",
-        "path":"Influx.html"
+        "path":"Influx.md"
     },
     {
         "name":"What can we do by using WAMPFramework",
         "time":"9/5/2019",
-        "path":"wamp.html"
+        "path":"wamp.md"
     }
 ]
