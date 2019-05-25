cd2098808cfb0dfe6ef4507cfadfef2835bbd87d
jiangdunchun <jiangdunchun@outlook.com>
2019-05-25 16:28:45 +0800

    use html for blogs

--- json/blogs.js
+++ json/blogs.js
@@ -2,11 +2,11 @@ BlogsCatalog = [
     {
         "name":"Some Tips of Using InfluxDB",
         "time":"21/5/2019",
-        "path":"Influx.md"
+        "path":"Influx.html"
     },
     {
         "name":"What can we do by using WAMPFramework",
         "time":"9/5/2019",
-        "path":"wamp.js"
+        "path":"wamp.html"
     }
 ]
