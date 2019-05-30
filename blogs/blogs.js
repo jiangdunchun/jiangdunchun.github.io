26083ec690480eb9bd0efcf6e98ffc6516e6696a
JiangDunchun <jiangdunchun@outlook.com>
2019-05-31 00:06:08 +0800

    add new blog

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -1,5 +1,10 @@
 BlogsCatalog = [
     {
+        "name":"Using WebAssembly in Decoding H.264 for Real-time Remote Rendering",
+        "time":"1/6/2019",
+        "path":"WebAssembly.md"
+    },
+    {
         "name":"WampFramework: Remotely Invoke the API in Local C# Assembly from js",
         "time":"26/5/2019",
         "path":"WampFramework.md"
