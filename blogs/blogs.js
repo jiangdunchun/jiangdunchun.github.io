241c5042cff69419298c313d80b4e3530fc404c7
jiangdunchun <jiangdunchun@outlook.com>
2019-05-31 15:59:28 +0800

    change the name

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -7,6 +7,6 @@ BlogsCatalog = [
     {
         "name":"WampFramework: Remotely Invoke the API in Local C# Assembly from js",
         "time":"26/5/2019",
-        "path":"WampFramework_Remotely_Invoke_the_API_in_Local_C#_Assembly_from_js.md"
+        "path":"WampFramework_Remotely_Invoke_the_API_in_Local_CSharp_Assembly_from_js.md"
     }
 ]
