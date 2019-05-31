a15d6b386cea1d919c8e1590b5279d31f0c5348f
jiangdunchun <jiangdunchun@outlook.com>
2019-05-31 15:14:13 +0800

    change the name

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -2,11 +2,11 @@ BlogsCatalog = [
     {
         "name":"Using WebAssembly in Decoding H.264 for Real-time Remote Rendering",
         "time":"1/6/2019",
-        "path":"WebAssembly.md"
+        "path":"Using_WebAssembly_in_Decoding_H.264_for_Real-time_Remote_Rendering.md"
     },
     {
         "name":"WampFramework: Remotely Invoke the API in Local C# Assembly from js",
         "time":"26/5/2019",
-        "path":"WampFramework.md"
+        "path":"WampFramework_Remotely_Invoke_the_API_in_Local_C#_Assembly_from_js.md"
     }
 ]
