3e5dbef8423d762e8201d0deaf96d8c48fa435d1
jiangdunchun <jiangdunchun@outlook.com>
2022-12-31 12:34:52 +0800

    add missing file

---
+++ blogs.js
@@ -0,0 +1,27 @@
+BlogsCatalog = [
+    {
+        "name":"基于FFT的海面渲染",
+        "time":"25/9/2021",
+        "path":"fft_water_simulation.md"
+    },
+    {
+        "name":"WebRTC in IPC Live & Remote Rendering",
+        "time":"10/10/2020",
+        "path":"WebRTC_in_IPC_Live_&_Remote_Rendering.md"
+    },
+    {
+        "name":"A Simple MR Application about Digital Twins and It's Great Expectation",
+        "time":"7/6/2019",
+        "path":"A_Simple_MR_Application_about_Digital_Twins_and_It's_Great_Expectation.md"
+    },
+    {
+        "name":"Using WebAssembly in Decoding H.264 for Real-time Remote Rendering",
+        "time":"1/6/2019",
+        "path":"Using_WebAssembly_in_Decoding_H.264_for_Real-time_Remote_Rendering.md"
+    },
+    {
+        "name":"WampFramework: Remotely Invoke the API in Local C# Assembly from js",
+        "time":"26/5/2019",
+        "path":"WampFramework_Remotely_Invoke_the_API_in_Local_CSharp_Assembly_from_js.md"
+    }
+]
