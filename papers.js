e5329966c2a9e6df7183b10caa8c5b1bf8be1e64
JiangDunchun <jiangdunchun@outlook.com>
2023-09-07 21:15:01 +0800

    add papers

---
+++ papers.js
@@ -0,0 +1,10 @@
+PapersCatalog = [
+    {
+        "name":"NeRF Representing Scenes as Neural Radiance Fields for View Synthesis",
+        "path":"NeRF_Representing_Scenes_as_Neural_Radiance_Fields_for_View_Synthesis.pdf"
+    },
+    {
+        "name":"Phase-Functioned Neural Networks for Character Control",
+        "path":"Phase-Functioned_Neural_Networks_for_Character_Control.pdf"
+    }
+]
