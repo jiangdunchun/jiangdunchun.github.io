38d6b1a0ae6cd5c114134294ad5d53af1176ef16
JiangDunchun <jiangdunchun@outlook.com>
2023-10-05 11:34:41 +0800

    change the css file

--- papers.js
+++ papers.js
@@ -1,6 +1,6 @@
 PapersCatalog = [
     {
-        "name":"NeRF Representing Scenes as Neural Radiance Fields for View Synthesis",
+        "name":"NeRF:Representing Scenes as Neural Radiance Fields for View Synthesis",
         "path":"NeRF_Representing_Scenes_as_Neural_Radiance_Fields_for_View_Synthesis.pdf"
     },
     {
