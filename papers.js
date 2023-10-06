82b88144a1484510ecda430a221f65ac4014ab3f
JiangDunchun <jiangdunchun@outlook.com>
2023-10-06 16:08:15 +0800

    add new papers

--- papers.js
+++ papers.js
@@ -6,5 +6,17 @@ PapersCatalog = [
     {
         "name":"Phase-Functioned Neural Networks for Character Control",
         "path":"Phase-Functioned_Neural_Networks_for_Character_Control.pdf"
+    },
+    {
+        "name":"Visually Lossless Content and Motion Adaptive Shading in Games",
+        "path":"Visually_Lossless_Content_and_Motion_Adaptive_Shading_in_Games.pdf"
+    },
+    {
+        "name":"Surface Simplification Using Quadric Error Metrics",
+        "path":"Surface_Simplification_Using_Quadric_Error_Metrics.pdf"
+    },
+    {
+        "name":"Simplifying Surfaces with Color and Texture using Quadric Error Metrics",
+        "path":"Simplifying_Surfaces_with_Color_and_Texture_using_Quadric_Error_Metrics.pdf"
     }
 ]
